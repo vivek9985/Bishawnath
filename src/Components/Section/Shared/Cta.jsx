b84ce004ec4a9dacc9b2e40304dc6b50668616e4
vivek9985 <vivekanando363@gmail.com>
2025-06-26 13:24:15 +0600

    modified cta section

--- src/Components/Section/Shared/Cta.jsx
+++ src/Components/Section/Shared/Cta.jsx
@@ -6,20 +6,17 @@ import { Link } from "react-router-dom";
 const Cta = () => {
     return (
         <section className="pb-40">
-            <div className="w-[1200px] h-[600px] mx-auto rounded-[50px] overflow-hidden relative">
+            <div className="w-11/12 sm:w-9/12 md:w-[715px] lg:w-[900px] xl:w-[1200px] h-[330px] sm:h-[350px] md:h-[400px] lg:h-[500px] xl:h-[600px] mx-auto rounded-2xl sm:rounded-3xl md:rounded-[50px] bg-stone-300 dark:bg-stone-700 overflow-hidden relative">
                 <video
-                    className="w-full hidden dark:block invert"
+                    className="w-full h-auto aspect-video hidden md:dark:block invert"
                     loop
                     muted
                     autoPlay
                 >
-                    <source
-                        src={Bg}
-                        type="video/mp4"
-                    />
+                    <source src={Bg} type="video/mp4" />
                 </video>
                 <video
-                    className="w-full block dark:hidden"
+                    className="w-full h-auto aspect-video hidden md:block dark:hidden"
                     loop
                     muted
                     autoPlay
@@ -29,14 +26,14 @@ const Cta = () => {
                         type="video/mp4"
                     />
                 </video>
-                <div className="w-full h-full absolute left-0 top-0 bg-white/20 dark:bg-[#0000008b] flex items-center justify-center">
+                <div className="w-full h-full px-4 md:px-5 lg:px-0 absolute left-0 top-0 bg-white/20 dark:bg-[#0000008b] flex items-center justify-center">
                     <div className="text-black dark:text-white text-center outfit">
-                        <h2 className="text-[56px] font-light leading-[120%]">
+                        <h2 className="text-[32px] md:text-[48px] lg:text-[56px] font-light leading-[120%]">
                             Ready to <span className="lobster text-accent dark:text-primary">Supercharge</span>
                             <br />
                             Your web application?
                         </h2>
-                        <p className="text-xl tracking-wide mt-5 mb-10">Let&apos;s Build Together</p>
+                        <p className="text-base sm:text-base md:text-xl tracking-wide mt-3 md:mt-5 mb-8 md:mb-10">Let&apos;s Build Together</p>
                         <Link to="/contact" className="w-fit block mx-auto"><PrimaryButton text="Let's Talk" /></Link>
                     </div>
                 </div>
