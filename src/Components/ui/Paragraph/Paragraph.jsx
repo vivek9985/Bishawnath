646cb7170ed4cc0b91915da5fef936f7162a2ecd
vivek9985 <vivekanando363@gmail.com>
2025-07-01 18:09:56 +0600

    modified paragraph

--- src/Components/ui/Paragraph/Paragraph.jsx
+++ src/Components/ui/Paragraph/Paragraph.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-
 const Paragraph = ({ children, className }) => {
     return (
         <p className={`font-medium dark:text-primary outfit text-base md:text-xl ${className}`}>
