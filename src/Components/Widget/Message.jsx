e0e3a8b061beb37059d88357a6e299018f059b9f
vivek9985 <vivekanando363@gmail.com>
2025-07-14 23:39:32 +0600

    added animation in Message compo

--- src/Components/Widget/Message.jsx
+++ src/Components/Widget/Message.jsx
@@ -4,50 +4,59 @@ import MessageIcon from "../../assets/front/premium.png";
 import MessageIconDark from "../../assets/front/gradient.png";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaFacebookMessenger } from "react-icons/fa";
+import { useGSAP } from "@gsap/react";
 
 
 const Message = () => {
+    const messageRef = useRef(null);
     const hiRef = useRef(null);
     const helpRef = useRef(null);
     const wRef = useRef(null);
     const mRef = useRef(null);
     const [shown, setShown] = useState(false);
 
+    useGSAP(() => {
+        gsap.from(
+            messageRef.current,
+            { x: 200, opacity: 0, duration: 0.6, delay: 4, ease: "power4.inOut" },
+        );
+    })
+
     const handleClick = () => {
         setShown(!shown)
         if (!shown) {
             gsap.to(
                 hiRef.current,
-                { scale: 0 },
+                { scaleY: 0, x: -50, opacity: 0 },
             );
             gsap.to(
                 helpRef.current,
-                { scale: 0 },
+                { scaleY: 0, x: -50, opacity: 0, delay: 0.2 },
             );
             gsap.to(
                 wRef.current,
-                { scale: 1 },
+                { scale: 1, delay: 0.3 },
             );
             gsap.to(
                 mRef.current,
-                { scale: 1 },
+                { scale: 1, delay: 0.5 },
             );
         } else {
             gsap.to(
                 hiRef.current,
-                { scale: 1 },
+                { scaleY: 1, x: 0, opacity: 1, delay: 0.3 },
             );
             gsap.to(
                 helpRef.current,
-                { scale: 1 },
+                { scaleY: 1, x: 0, opacity: 1, delay: 0.4 },
             );
             gsap.to(
                 wRef.current,
-                { scale: 0 },
+                { scale: 0, delay: 0.2 },
             );
             gsap.to(
                 mRef.current,
-                { scale: 0 },
+                { scale: 0, delay: 0.1 },
             );
         }
     };
@@ -55,32 +64,33 @@ const Message = () => {
     return (
         <div
             onClick={handleClick}
-            className="w-12 h-12 rounded-full flex items-center justify-center dark:bg-primary bg-purple-500 cursor-pointer fixed right-10 bottom-10 z-50"
+            ref={messageRef}
+            className="w-11 h-11 rounded-full flex items-center justify-center dark:bg-primary bg-purple-500 cursor-pointer fixed right-10 bottom-10 z-50"
         >
             <div className="w-full h-full relative flex items-center justify-center">
                 <img className="w-8 block dark:hidden" src={MessageIcon} alt="Message Icon" />
                 <img className="w-8 hidden dark:block" src={MessageIconDark} alt="Message Icon" />
                 <div
                     ref={helpRef}
-                    className="w-[100px] bg-purple-500 dark:bg-primary rounded-full px-3 py-2 text-white dark:text-black outfit font-medium absolute -top-[98px] right-0"
+                    className="w-[100px] bg-purple-500 dark:bg-primary rounded-full px-3 py-1.5 text-white dark:text-black outfit font-medium absolute -top-[95px] right-0"
                 >
                     Hi there !
                 </div>
                 <div
                     ref={hiRef}
-                    className="w-[170px] bg-purple-500 dark:bg-primary rounded-full px-3 py-2 text-white dark:text-black outfit font-medium absolute -top-12 right-0"
+                    className="w-[170px] bg-purple-500 dark:bg-primary rounded-full px-3 py-1.5 text-white dark:text-black outfit font-medium absolute -top-12 right-0"
                 >
                     How can I help you ?
                 </div>
                 <div
                     ref={wRef}
-                    className="scale-0 w-12 h-12 flex items-center justify-center bg-purple-500 dark:bg-primary rounded-full text-white dark:text-black outfit font-medium absolute top-0 right-[55px]"
+                    className="scale-0 w-11 h-11 flex items-center justify-center bg-purple-500 dark:bg-primary rounded-full text-white dark:text-black outfit font-medium absolute top-[-55px] right-0"
                 >
                     <FaWhatsapp size={20} />
                 </div>
                 <div
                     ref={mRef}
-                    className="scale-0 w-12 h-12 flex items-center justify-center bg-purple-500 dark:bg-primary rounded-full text-white dark:text-black outfit font-medium absolute top-0 right-[110px]"
+                    className="scale-0 w-11 h-11 flex items-center justify-center bg-purple-500 dark:bg-primary rounded-full text-white dark:text-black outfit font-medium absolute top-[-110px] right-0"
                 >
                     <FaFacebookMessenger size={20} />
                 </div>
